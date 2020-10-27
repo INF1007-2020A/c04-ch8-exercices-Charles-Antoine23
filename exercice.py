c9368e7a5bd4706d63b0c9e7f3007cd69c3c71b6
Charles-Antoine <70984285+Charles-Antoine23@users.noreply.github.com>
2020-10-27 18:24:58 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -20,13 +20,28 @@ def exercice1(file1, file2):
                 print(line2)
                 break
 
-def exercice2(file1):
-    with open(file1, encoding="utf-8") as f1:
+def exercice2(input_file, output_file):
+    with open(input_file, encoding="utf-8") as in_file, open(output_file, 'w', encoding="utf-8") as out_file:
+        for line in in_file:
+            words = line.split()
+            line_triple = "   ".join(words)
+            out_file.write(line_triple)
+
+def exercice3(file_path, result_file_path):
+    with open(file_path, encoding="utf-8") as f:
+        note_percent = f.readlines()
+
+    with open(result_file_path, 'w', encoding="utf-8"):
+        for note in note_percent:
+            for key, value in PERCENTAGE_TO_LETTER.items():
+                if value[0] <= int(note) < value[1]:
+                    f.write(note + " " + key)
+                    break
 
 
 
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-
+    exercice3("./notes.txt", "./notes_letter.txt")
     pass
